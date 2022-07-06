a5df2227fd7bdf90d153a4f46ce2cfa307f2c085
Mayank Kumar <mj112295@gmail.com>
2022-07-06 06:47:43 +0530

    Initial Commit

---
+++ src/main/java/com/knackitsolutions/profilebaba/isperp/IsperpApplication.java
@@ -0,0 +1,13 @@
+package com.knackitsolutions.profilebaba.isperp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsperpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsperpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knackitsolutions/profilebaba/isperp/IsperpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knackitsolutions.profilebaba.isperp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsperpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
